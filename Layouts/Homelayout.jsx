7e5e7b3adc55c04ddcc1ce2db89f58cbf1183f79
ahasan-ullah <ahasanullah451@gmail.com>
2024-11-15 23:28:41 +0600

    navbar added

--- Layouts/Homelayout.jsx
+++ Layouts/Homelayout.jsx
@@ -1,16 +1,25 @@
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
-const Homelayout=()=>{
-  return(
+const Homelayout = () => {
+  return (
     <div className="font-poppins">
-    <header>
-      <Header></Header>
-      <section className="w-11/12 mx-auto">
-        <LatestNews></LatestNews>
-      </section>
-    </header>
+      <header>
+        <Header></Header>
+        <section className="w-11/12 mx-auto">
+          <LatestNews></LatestNews>
+        </section>
+      </header>
+      <nav className="w-11/12 mx-auto py-2">
+        <Navbar></Navbar>
+      </nav>
+      <main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+        <aside className="col-span-3">Left</aside>
+        <section className="col-span-6">Main</section>
+        <aside className="col-span-3">Right</aside>
+      </main>
     </div>
   );
-}
+};
 export default Homelayout;
