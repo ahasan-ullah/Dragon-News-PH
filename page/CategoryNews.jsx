8e72d753ece62b619311b424f83e4ee5ab0b1ecf
ahasan-ullah <ahasanullah451@gmail.com>
2024-11-16 01:47:32 +0600

    news card added

--- page/CategoryNews.jsx
+++ page/CategoryNews.jsx
@@ -1,11 +1,17 @@
 import { useLoaderData } from "react-router-dom";
+import NewsCard from "../components/NewsCard";
 
 const CategoryNews = () => {
-  const { data } = useLoaderData();
-  console.log(data);
+  const { data: news } = useLoaderData();
   return(
     <div>
-      <h2 className="font-semibold">Dragon News Home</h2>
+      <h2 className="font-semibold mb-3">Dragon News Home</h2>
+      <p className="text-gray-400 text-sm">{news.length} news found on this category</p>
+      <div className="space-y-5 mt-5">
+        {
+          news.map(singleNews=> <NewsCard key={singleNews._id} news={singleNews}></NewsCard>)
+        }
+      </div>
     </div>
   );
 };
